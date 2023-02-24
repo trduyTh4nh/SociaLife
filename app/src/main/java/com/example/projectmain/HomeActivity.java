86fe30ee197d2eaf7c2b4ee5967731b855790488
Duy Thanh <95486959+DuyThanh2003@users.noreply.github.com>
2023-02-24 10:26:29 +0700

    test up code on github

---
+++ app/src/main/java/com/example/projectmain/HomeActivity.java
@@ -0,0 +1,79 @@
+package com.example.projectmain;
+
+import static android.app.PendingIntent.getActivity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class HomeActivity extends AppCompatActivity {
+    BottomNavigationView navigationView;
+    FloatingActionButton btnAdd;
+
+    Toolbar toolbar;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        // this is line hide
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        navigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.body_container, new HomeFragment()).commit();
+        navigationView.setSelectedItemId(R.id.nav_home);
+
+        btnAdd = findViewById(R.id.fab);
+        btnAdd .setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.body_container, new AddFragment()).commit();
+            }
+        });
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+
+                Fragment fragment = null;
+
+
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.nav_search:
+                        fragment = new SreachFragment();
+                        break;
+                    case R.id.nav_add:
+
+                        fragment = new AddFragment();
+                        break;
+                    case R.id.nav_notify:
+                        fragment = new NotifyFragment();
+                        break;
+                    case R.id.nav_user:
+                        fragment = new UserFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.body_container, fragment).commit();
+                return true;
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/projectmain/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.projectmain;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        BottomNavigationView navigationView = findViewById(R.id.bottom_nav);
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.action_home:
+                        Toast.makeText(MainActivity.this, "Home page", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_search:
+                        Toast.makeText(MainActivity.this, "Search user", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_add:
+                        Toast.makeText(MainActivity.this, "Add post", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_notify:
+                        Toast.makeText(MainActivity.this, "Notification layout", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_user:
+                        Toast.makeText(MainActivity.this, "User profile", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+}
