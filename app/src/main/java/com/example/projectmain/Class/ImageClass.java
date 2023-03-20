8a2643a4a483975f40de8171098299bf8b120add
fowardslash <68984861+fowardslash@users.noreply.github.com>
2023-03-20 21:14:03 +0700

    cải tiến giao diện

---
+++ app/src/main/java/com/example/projectmain/Class/ImageClass.java
@@ -0,0 +1,14 @@
+package com.example.projectmain.Class;
+
+public class ImageClass {
+    private int Resource;
+    public ImageClass(int r){
+        this.Resource = r;
+    }
+    public int getResource(){
+        return this.Resource;
+    }
+    public void setResource(int r){
+        Resource = r;
+    }
+}
--- app/src/main/java/com/example/projectmain/Fragment/UserFragment.java
+++ app/src/main/java/com/example/projectmain/Fragment/UserFragment.java
@@ -4,9 +4,12 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,9 +18,14 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.example.projectmain.Adapter.ImageAdapter;
+import com.example.projectmain.Class.ImageClass;
 import com.example.projectmain.Database.DB;
 import com.example.projectmain.R;
 import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 
 public class UserFragment extends Fragment {
@@ -35,6 +43,9 @@ public class UserFragment extends Fragment {
     private static final String KEY_EMAIL = "email";
     private static final String KEY_NAME = "name";
     private static final String KEY_PASSWORD = "password";
+    TextView fmTvPostCount;
+    int[] imageRes = {R.drawable.imgquang, R.drawable.meo, R.drawable.imgcrew, R.drawable.imgpc};
+    private ArrayList<ImageClass> list = new ArrayList<ImageClass>();
 
     Toolbar toolbar;
     DrawerLayout drawerLayout;
@@ -47,6 +58,10 @@ public class UserFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Random r = new Random();
+        for(int i = 1; i <= 20; i++){
+            list.add(new ImageClass(imageRes[r.nextInt(imageRes.length)]));
+        }
     }
 
 
@@ -61,22 +76,22 @@ public class UserFragment extends Fragment {
 
 
     @Override
-    public void onViewCreated( View view,Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        mtvUsername = view.findViewById(R.id.tvName);
-        mtvFollowingCount = view.findViewById(R.id.tvFollowing);
-//        mbtnLogout = view.findViewById(R.id.btnLogout);
-
-        db = new DB(getActivity());
-
-        sharedPreferences = getActivity().getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
-
-        String name = sharedPreferences.getString(KEY_NAME,null);
-
-        if(name != null){
-            mtvUsername.setText(name);
-        }
+        ImageAdapter adapter = new ImageAdapter(getActivity().getApplicationContext(), list);
+        RecyclerView r = getView().findViewById(R.id.rcvImages);
+        r.setNestedScrollingEnabled(false);
+        r.setAdapter(adapter);
+        GridLayoutManager g = new GridLayoutManager(getActivity().getApplicationContext(), 3, GridLayoutManager.VERTICAL, false){
+            @Override
+            public boolean canScrollVertically() {
+                return true;
+            }
+        };
+        r.setLayoutManager(g);
+        fmTvPostCount = getView().findViewById(R.id.tvPostCount);
+        fmTvPostCount.setText(String.valueOf(adapter.getItemCount()));
 
 //        mbtnLogout.setOnClickListener(new View.OnClickListener() {
 //            @Override
